31c557db4b455b7cce3f30a9f318c2b8b9ba6ad0
mari-cg <stompmarilene@outlook.com>
2025-08-19 16:57:12 +0200

    added wind speed and humidity

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ function refreshWeather(response) {
   let tempValue = document.querySelector(".temp-value");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+
+  console.log(response);
 
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   tempValue.innerHTML = Math.round(temperature);
 }
 
