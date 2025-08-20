0aeb454faf5b147b63a00b216442213a32bcc1a5
mari-cg <stompmarilene@outlook.com>
2025-08-20 21:34:55 +0200

    added current specs about the city

--- src/index.js
+++ src/index.js
@@ -5,14 +5,58 @@ function refreshWeather(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
-
-  console.log(response);
+  let timeElement = document.querySelector("#time");
+  let todayElement = document.querySelector("#today");
+  let date = new Date(response.data.time * 1000);
+  const { day, month, dates, hours, minutes } = formatDate(date);
+  let rawDescriptionElement = response.data.condition.description;
+  let capitalizedDescriptionElement =
+    rawDescriptionElement.charAt(0).toUpperCase() +
+    rawDescriptionElement.slice(1);
 
   cityElement.innerHTML = response.data.city;
-  descriptionElement.innerHTML = response.data.condition.description;
+  timeElement.innerHTML = `${month} ${dates}, ${hours}:${minutes}`;
+  todayElement.innerHTML = day;
+  descriptionElement.innerHTML = capitalizedDescriptionElement;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   tempValue.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let dates = date.getDate();
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const day = days[date.getDay()];
+  const month = months[date.getMonth()];
+
+  if (minutes < 10) minutes = `0${minutes}`;
+  if (hours < 10) hours = `0${hours}`;
+  return { day, month, dates, hours, minutes };
 }
 
 function searchCity(city) {
